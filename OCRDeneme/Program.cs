538a840bdaf01d3d81ad47fe31ceef30c73037a9
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2024-05-05 21:14:58 +0300

    Add project files.

---
+++ OCRDeneme/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+using IronOcr;
+using Newtonsoft.Json.Linq;
+
+try
+{
+    //// Initializes IronTesseract for OCR and reads text from an image file.
+    var ocr = new IronTesseract();
+    //iamge path
+    var result = ocr.Read("C:\\Users\\Bilgisayarim\\source\\pm\\OCRDeneme\\OCRDeneme\\bin\\Debug\\net8.0\\images\\market-fis.jpg");
+
+    // Extracts the text result from the OCR operation and splits it into lines.
+    string ocrResult = result.Text;
+    string[] lines = ocrResult.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+    // Creates a new JSON object using JObject from the Newtonsoft.Json library.
+    JObject jsonResponse = new JObject();
+
+    //loop is adjusted to start from 1 instead of 0 to begin indexing from 1.
+    for (int i = 1; i < lines.Length; i++)
+    {
+        jsonResponse[i.ToString()] = lines[i - 1];
+    }
+
+    // JSON Response
+    Console.WriteLine($"JSON Response:\n{jsonResponse}");
+
+    // Creates a new JSON object and adds the OCR text extracted from the result to it.
+    JObject jsonResult = new JObject();
+    jsonResult["OCRText"] = result.Text;
+
+    // JSON result
+    Console.WriteLine($"Json Result: \n{jsonResult}");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Error " + ex.Message);
+    throw;
+}
+
---
+++ deneme2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using Tesseract;
+using static TesserNet.Tesseract;
+
+using var engine = new TesseractEngine("./tessdata", "eng", EngineMode.Default);
+
+// JSON dosyasını okuyun (örneğin)
+string jsonContent = File.ReadAllText("path/to/your/json/file.json");
+JObject json = JObject.Parse(jsonContent);
+string description = json["description"].ToString();
+
+// Görüntüyü yükleyin ve OCR ile işleyin
+using (var image = Pix.LoadFromFile("path/to/your/image.jpg"))
+{
+    using (var page = engine.Process(image))
+    {
+        // Tanımlanan metni alın
+        var text = page.GetText();
+
+        // Görüntüden ve JSON'dan alınan metinleri birleştirin
+        string combinedText = CombineText(description, text);
+
+        // Çıktıyı ekrana yazdırın
+        Console.WriteLine(combinedText);
+    }
+}
+
+
+static string CombineText(string description, string ocrText)
+{
+    // JSON'dan alınan metni başlangıçta ekleyin
+    string combinedText = description + Environment.NewLine;
+
+    // OCR'dan alınan metni ekleyin
+    combinedText += ocrText;
+
+    return combinedText;
+}
+
